2f86525a1b4f136639e53976d0f0c19971c435fc
pistachio <tomomoto011@gmail.com>
2026-04-30 13:57:53 +0300

    добавлены категории заметок, inline удаление, фикс раскладки и подключена Neon DB 1

--- main.py
+++ main.py
@@ -772,7 +772,8 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())import os
+    asyncio.run(main())
+import os
 import asyncio
 import logging
 import re
